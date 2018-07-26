345d99234d061b1e74d272f7fcd947eea60f8b6a
github username <github email address>
2018-07-26 13:42:05 -0400

    Rendered a publisher has_many territories on congregations show page

--- app/assets/javascripts/territory.js
+++ app/assets/javascripts/territory.js
@@ -3,18 +3,35 @@ $(document).ready(function() {
 });
 //Requirement 2
 function attachListeners() {
-  $(".territories").on('click', function(e) {
+  $(".territories").one('click', function(e) {
     e.preventDefault()
 
     $.get(`http://localhost:3000/publishers.json`, function(data) {
       for (x in data) {
-        let t = document.createTextNode(`${data[x].first_name + " " + data[x].last_name}`)
-        let btn = document.createElement("BUTTON");
-        btn.appendChild(t);
+        let p = document.createTextNode(`${data[x].first_name + " " + data[x].last_name}`)
+        let btn = document.createElement("Button");
+        btn.appendChild(p);
+        btn.setAttribute("href", `${`http://localhost:3000/publishers/${data[x].id}.json`}`)
         document.body.appendChild(btn);
       };
+      //Requirement 4
+      $('button').click(function() {
+          $.get(($(this).attr("href")), function(data){
+            for (x in data.territories) {
+              if (data.territories[x].signed_out === "true"){
+                let t = document.createTextNode(`${data.territories[x].number + " " + data.territories[x].designation}`)
+                let btn = document.createElement("ol");
+                btn.appendChild(t);
+                document.body.appendChild(btn);
+              }
+            };;
+          });
+      });
     })
   })
+
+
+
   // const url = $(".new").attr("href").json
 
     $(".new").on('click', function(e) {
